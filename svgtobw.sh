d81233b4774136800566c6ebe8d62f66cc52a6d4
Humming Owl <64061464+Humming-Owl@users.noreply.github.com>
2022-04-26 23:27:54 -0400

    Add files via upload

--- svgtobw.sh
+++ svgtobw.sh
@@ -20,30 +20,26 @@ do
 
 	newname=$( echo $f | sed "s/input\///" | sed "s/.svg//" )
 
-	echo $int >> data.txt
-	echo "$int > $newname.svg"
+	echo $percentwhite >> data.txt
+	echo "$percentwhite > $newname.svg"
 
 	convert temp.bmp -colorspace gray -morphology edgeout:3 Disk -negate -transparent white -threshold 100% temp_extra.bmp
 
 	if (( $percentwhite >= 0 && $percentwhite <= 40 )); then
 
 		./magick temp.bmp -auto-threshold Triangle temp_threshold.bmp
-		convert temp.bmp -threshold 100% temp_background.bmp
-		composite temp_threshold.bmp temp_background.bmp final/$newname.bmp
-		composite temp_extra.bmp final/$newname.bmp final/$newname.bmp
-		convert final/$newname.bmp -gravity center -extent 770x770 -morphology Erode Disk:4 final/$newname.bmp
-		potrace --height 2048pt --width 2048pt -s final/$newname.bmp -o final/$newname.svg
 
 	elif (( $percentwhite > 40 && $percentwhite <= 100 )); then
 
 		./magick temp.bmp -auto-threshold OTSU temp_threshold.bmp
-		convert temp.bmp -threshold 100% temp_background.bmp
-		composite temp_threshold.bmp temp_background.bmp final/$newname.bmp
-		composite temp_extra.bmp final/$newname.bmp final/$newname.bmp
-		convert final/$newname.bmp -gravity center -extent 770x770 -morphology Erode Disk:4 final/$newname.bmp
-		potrace --height 2048pt --width 2048pt -s final/$newname.bmp -o final/$newname.svg
 
 	fi
+
+	convert temp.bmp -threshold 100% temp_background.bmp
+	composite temp_threshold.bmp temp_background.bmp final/$newname.bmp
+	composite temp_extra.bmp final/$newname.bmp final/$newname.bmp
+	convert final/$newname.bmp -gravity center -extent 752x752 -morphology Erode Disk:4 final/$newname.bmp
+	potrace --height 2048pt --width 2048pt -s final/$newname.bmp -o final/$newname.svg
 
 	percentwhite2=$( convert final/$newname.bmp -fuzz 60% -fill white -opaque white -fill black +opaque white -format "%[fx:100*mean]" info: | sed "s/\..*//" )
 
